19ea6868738bc98a59abfeb9f23dd311f2db413a
sujit gargate <sujitgargate5893@gmail.com>
2020-08-17 02:42:59 +0530

    Added : Person Data Editing Added

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,21 +1,158 @@
+import java.util.*;
+
 public class AddressBook {
 
+   String firstName;
+   String lastName;
+   String address;
+   String city;
+   String state;
+   int zipCode;
+   int phoneNumber;
+   private static Scanner sc = new Scanner(System.in);
+
    public static void main(String[] args) {
-      System.out.println("Welcome To AddressBook");
+      AddressBook addressBookObj = new AddressBook();
+      System.out.println("Welcome to AddressBook");
+
+      //Initialization of map
+      Map<String, List<Object>> addressBook = new TreeMap<>();
+
+      System.out.println("Enter options \n1.Add Person \n2.Edit Person");
+      int choiceOfOperation = sc.nextInt();
+
+      switch (choiceOfOperation) {
+         case 1:
+            addressBookObj.addPerson(addressBook);
+            break;
+
+         case 2:
+            addressBookObj.editingAddressBook(addressBook);
+            break;
+
+         default:
+            System.out.println("Enter valid Option");
+            break;
+      }
    }
 
-   String personFirstName, personLastName, personAddress, personCity, personState;
-   int personZip, personPhoneNumber;
+   /**
+    * ADDPERSON RETURNS HASH MAP AND ALSO TAKES PARAMETERS HASH MAP
+    * 1ST IS NAME OF PERSON AND 2ND IS LIST OF OTHER DETAILS
+    */
+   public Map<String, List<Object>> addPerson(Map<String, List<Object>> addressBook) {
 
-   AddressBook(String personFirstName, String personLastName, String personAddress,
-               String personCity, String personState, int personZip, int personPhoneNumber) {
+      System.out.print("Enter The First Name :- \n");
+      sc.nextLine();
+      String firstName = sc.next();
 
-      this.personFirstName = personFirstName;
-      this.personLastName = personLastName;
-      this.personAddress = personAddress;
-      this.personCity = personCity;
-      this.personState = personState;
-      this.personZip = personZip;
-      this.personPhoneNumber = personPhoneNumber;
+      System.out.print("Enter The Last Name :- \n");
+      String lastName = sc.next();
+
+      System.out.print("Enter The address :- \n");
+      String address = sc.next();
+
+      System.out.print("Enter The City :- \n");
+      String city = sc.next();
+
+      System.out.print("Enter The State :- \n");
+      String state = sc.next();
+
+      System.out.print("Enter The Zip Code :- \n");
+      int zipCode = sc.nextInt();
+
+      System.out.print("Enter The Phone Number :- \n");
+      int phoneNumber = sc.nextInt();
+
+      //Creating list For Person details
+      List<Object> personData = new ArrayList<>();
+      personData.add(firstName);
+      personData.add(lastName);
+      personData.add(address);
+      personData.add(city);
+      personData.add(state);
+      personData.add(zipCode);
+      personData.add(phoneNumber);
+
+      String nameofPerson = firstName + "" + lastName;
+
+      //PesonData is list
+      addressBook.put(nameofPerson, personData);
+      displayAddressBook(addressBook, nameofPerson);
+      return addressBook;
+   }
+
+   //Displaying Person Data
+   public void displayAddressBook(Map<String, List<Object>> addressBook, String nameOfPerson) {
+      if (addressBook.containsKey(nameOfPerson)) {
+         System.out.println("The Person Data is : " + addressBook.get(nameOfPerson));
+      }
+   }
+
+   //For Editing Existing Person Data
+   private Map<String, List<Object>> editingAddressBook(Map<String, List<Object>> addressBook) {
+
+      Map<String, List<Object>> addPersonForEditing = addPerson(addressBook);
+      System.out.println("Enter Name To Edit Person Data");
+      String findPersonName = sc.next();
+
+      //Checking If The Name Is There
+      if (addPersonForEditing.containsKey(findPersonName) != false) {
+
+         //Finding name from addressBook
+         List<Object> personName = new ArrayList<>(addPersonForEditing.get(findPersonName));
+
+         System.out.println("Choose Below To Edit");
+         System.out.println("\n 1. Address");
+         System.out.println("\n 2. City");
+         System.out.println("\n 3. State");
+         System.out.println("\n 4. Zip");
+         System.out.println("\n 5. Phone");
+
+         int choice = sc.nextInt();
+
+         switch (choice) {
+            case 1:
+               System.out.println("Enter Address");
+               String editAddress = sc.next();
+               personName.set(2, editAddress);
+               addressBook.replace(findPersonName, personName);
+               break;
+            case 2:
+               System.out.println("Enter City");
+               String editCity = sc.next();
+               personName.set(3, editCity);
+               addressBook.replace(findPersonName, personName);
+               break;
+            case 3:
+               System.out.println("Enter State");
+               String editState = sc.next();
+               personName.set(4, editState);
+               addressBook.replace(findPersonName, personName);
+               break;
+            case 4:
+               System.out.println("Enter Zip Code");
+               int editZip = sc.nextInt();
+               personName.set(5, editZip);
+               addressBook.replace(findPersonName, personName);
+               break;
+            case 5:
+               System.out.println("Enter Phone Number");
+               int editPhone = sc.nextInt();
+               personName.set(6, editPhone);
+               addressBook.replace(findPersonName, personName);
+               break;
+            default:
+               System.out.println("Enter Valid Option");
+         }
+
+         displayAddressBook(addressBook, findPersonName);
+         return addressBook;
+      }
+      else {
+         System.out.println("Entered Name Not Found");
+         return null;
+      }
    }
 }
+
--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class Person {
+
+   String firstName;
+   String lastName;
+   String address;
+   String city;
+   String state;
+   Integer zip;
+   Integer phone;
+/*
+   Person() {
+   }*/
+
+   Person(String firstName, String lastName, String address, String city, String state, Integer zip,
+          Integer phone) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.address = address;
+      this.city = city;
+      this.state = state;
+      this.zip = zip;
+      this.phone = phone;
+   }
 }
